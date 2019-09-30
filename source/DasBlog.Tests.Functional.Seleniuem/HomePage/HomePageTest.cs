fabe72f909ad121b2626db64d0dd73a7b98908a5
Mark Downie <poppastring@users.noreply.github.com>
2019-09-29 21:27:23 -0400

    Simplyfying Selenium usage...

---
+++ source/DasBlog.Tests.Functional.Seleniuem/HomePage/HomePageTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using DasBlog.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using Xunit;
+
+namespace DasBlog.Tests.Functional.Selenium
+{
+	public class HomePageTest : IClassFixture<SeleniumServerFactory<Startup>>, IDisposable
+	{
+		public SeleniumServerFactory<Startup> Server { get; }
+		public IWebDriver Browser { get; }
+		public HttpClient Client { get; }
+		public ILogs Logs { get; }
+
+		public HomePageTest(SeleniumServerFactory<Startup> server)
+		{
+			Server = server;
+			Client = server.CreateClient(); //weird side effecty thing here. This call shouldn't be required for setup, but it is.
+
+			var opts = new ChromeOptions();
+			opts.AddArgument("--headless"); //Optional, comment this out if you want to SEE the browser window
+			opts.SetLoggingPreference(OpenQA.Selenium.LogType.Browser, LogLevel.All);
+
+			var driver = new RemoteWebDriver(opts);
+			Browser = driver;
+			Logs = new RemoteLogs(driver); //TODO: Still not bringing the logs over yet
+		}
+
+		[Fact]
+		[Trait("Home", "FunctionalTest")]
+		public void LoadTheMainPageAndCheckTitle()
+		{
+			Browser.Navigate().GoToUrl(Server.RootUri);
+			Assert.StartsWith("My DasBlog!", Browser.Title);
+		}
+
+		[Fact]
+		[Trait("Home", "FunctionalTest")]
+		public void ThereIsAnH1()
+		{
+			Browser.Navigate().GoToUrl(Server.RootUri);
+
+			var headerSelector = By.TagName("h1");
+			Assert.Equal("HANSELMINUTES PODCAST\r\nby Scott Hanselman", Browser.FindElement(headerSelector).Text);
+		}
+
+		[Fact]
+		[Trait("Home", "FunctionalTest")]
+		public void KevinScottTestThenGoHome()
+		{
+			Browser.Navigate().GoToUrl(Server.RootUri + "/631/how-do-you-become-a-cto-with-microsofts-cto-kevin-scott");
+
+			var headerSelector = By.TagName("h1");
+			var link = Browser.FindElement(headerSelector);
+			link.Click();
+			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri); //WTF
+		}
+
+		public void Dispose()
+		{
+			Browser.Dispose();
+		}
+	}
+}
---
+++ source/DasBlog.Tests.Functional.Seleniuem/SeleniumServerFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using DasBlog.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace DasBlog.Tests.Functional.Selenium
+{
+	public class SeleniumServerFactory<TStartup> : WebApplicationFactory<Startup> where TStartup : class
+	{
+		public string RootUri { get; set; } //Save this use by tests
+
+		Process _process;
+		IWebHost _host;
+
+		public SeleniumServerFactory()
+		{
+			ClientOptions.BaseAddress = new Uri("https://localhost"); //will follow redirects by default
+
+			_process = new Process()
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "selenium-standalone",
+					Arguments = "start",
+					UseShellExecute = true
+				}
+			};
+			_process.Start();
+		}
+
+		protected override TestServer CreateServer(IWebHostBuilder builder)
+		{
+			//Real TCP port
+			_host = builder.Build();
+			_host.Start();
+			RootUri = _host.ServerFeatures.Get<IServerAddressesFeature>().Addresses.LastOrDefault(); //Last is https://localhost:5001!
+
+			//Fake Server we won't use...this is lame. Should be cleaner, or a utility class
+			return new TestServer(new WebHostBuilder().UseStartup<TStartup>());
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			if (disposing)
+			{
+				_host.Dispose();
+				_process.CloseMainWindow(); //Be sure to stop Selenium Standalone
+			}
+		}
+	}
+}
