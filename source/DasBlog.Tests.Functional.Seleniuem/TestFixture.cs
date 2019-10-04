8d2d52cd40bcfeff21e0349a19104675d5ce1518
Mark Downie <poppastring@users.noreply.github.com>
2019-10-03 23:01:40 -0400

    test

---
+++ source/DasBlog.Tests.Functional.Seleniuem/TestFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace DasBlog.Tests.Functional.Selenium
+{
+	public class TestFixture : IDisposable
+	{
+		private Process _process;
+		public IWebDriver WebDriver;
+
+
+		public TestFixture()
+		{
+			StartServer();
+		}
+
+		private void StartServer()
+		{
+			string projectname = "";
+			string applicationPath = "";
+
+			_process = new Process
+			{
+
+			};
+
+			_process.Start();
+			// WebDriver = W
+		}
+
+		public void Dispose()
+		{
+			WebDriver.Dispose();
+			if (!_process.HasExited)
+			{
+				_process.Kill();
+			}
+		}
+	}
+}
